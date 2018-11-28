c9e9b409f3e87f544f6fdc2fc6e5971a03a4cd5f
i.akhaltsev <i.akhaltsev@tinkoff.ru>
2018-11-28 21:39:46 +0300

    hw oop BST add fold, foreach, toList[BSTLevel]

--- src/main/scala/lectures/oop/BST.scala
+++ src/main/scala/lectures/oop/BST.scala
@@ -36,9 +36,15 @@ trait BST {
 
   def find(value: Int): Option[BST]
 
-  def prefix_traverse: Int
+  def fold(agg: Int)(f: (Int, Int) => Int): Int
+
+  def foreach[U](f: Any => U): U
+
+  def toList: List[BSTLevel]
 
 }
+
+case class BSTLevel(bst: BST, level: Int)
 
 object BST {
 
@@ -53,6 +59,7 @@ object BST {
   def apply(root: BST, list: List[Int]): BST = recurse(root, list)
 
   def apply(root: BST, elems: Int*): BST = recurse(root, elems.toList)
+
 }
 
 case class BSTImpl(value: Int,
@@ -78,45 +85,97 @@ case class BSTImpl(value: Int,
     case BSTImpl(`value`, _, _)                    => Some(this)
     case BSTImpl(root, Some(l), _) if value < root => l.find(value)
     case BSTImpl(root, _, Some(r)) if value > root => r.find(value)
-    case _                                         => None
   }
 
-  def prefix_traverse: Int = this match {
-    case BSTImpl(`value`, _, _) => `value`
-    case BSTImpl(_, Some(l), _) => l.prefix_traverse
+  def fold(agg: Int)(f: (Int, Int) => Int): Int = this match {
+    case BSTImpl(v, None, None)       => v
+    case BSTImpl(v, Some(l), Some(r)) => f(l.fold(agg)(f), f(v, r.fold(agg)(f)))
+    case BSTImpl(v, Some(l), None)    => f(v, l.fold(agg)(f))
+    case BSTImpl(v, None, Some(r))    => f(v, r.fold(agg)(f))
   }
 
-  override def toString: String =
-    value + "\n" + "[l=" + left.getOrElse("Empty") + "][ r=" + right.getOrElse(
-      "Empty") + "]"
+  def foreach[U](f: Any => U): U = this match {
+    case BSTImpl(v, None, None) => f(v)
+    case BSTImpl(v, Some(l), Some(r)) =>
+      f(v)
+      l.foreach(f)
+      r.foreach(f)
+    case BSTImpl(v, Some(l), None) =>
+      f(v)
+      l.foreach(f)
+    case BSTImpl(v, None, Some(r)) =>
+      f(v)
+      r.foreach(f)
+  }
+
+  def toList: List[BSTLevel] = {
+    def go(bst: BST,
+           level: Int,
+           acc: List[BSTLevel] = List.empty[BSTLevel]): List[BSTLevel] =
+      bst match {
+        case bst @ BSTImpl(_, Some(l), Some(r)) =>
+          go(r, level + 1, go(l, level + 1, acc :+ BSTLevel(bst, level)))
+        case bst @ BSTImpl(_, Some(l), None) =>
+          go(l, level + 1, acc :+ BSTLevel(bst, level))
+        case bst @ BSTImpl(_, None, Some(r)) =>
+          go(r, level + 1, acc :+ BSTLevel(bst, level))
+        case bst @ BSTImpl(_, None, None) =>
+          acc :+ BSTLevel(bst, level)
+
+        case _ => acc
+      }
+
+    go(this, 1)
+  }
+
+  //  override def toString: String = {
+  //    println(this.foreach(identity))
+  //    ""
+  //    def stringify(bst: BST, level: Int): Int = this match {
+  //      case BSTImpl(_, Some(l), Some(r)) => Math.max(stringify(l, level + 1), stringify(r, level + 1))
+  //      case BSTImpl(_, Some(l), _) => stringify(l, level + 1)
+  //      case BSTImpl(_, _, Some(r)) => stringify(r, level + 1)
+  //      case _                      => level
+  //    }
+  //
+  //    stringify(this, 0)
+  //  }
+
+  //    value + "\n" + "[l=" + left.getOrElse("Empty") + "][ r=" + right.getOrElse(
+  //      "Empty") + "]"
+
 }
 
 object TreeTest extends App {
 
-  val sc = new java.util.Scanner(System.in)
+  //  val sc = new java.util.Scanner(System.in)
   val maxValue = 110000
-  val nodesCount = sc.nextInt()
+  //  val nodesCount = sc.nextInt()
 
   val markerItem = (Math.random() * maxValue).toInt
   val markerItem2 = (Math.random() * maxValue).toInt
   val markerItem3 = (Math.random() * maxValue).toInt
 
   // Generate huge tree
-  val root: BST = BSTImpl(maxValue / 2)
+  //  val root: BST = BSTImpl(maxValue / 2)
+  val root: BST = BST(100)
 
   // generator goes here
-  val generatedNodes = List.fill(nodesCount)((Math.random() * maxValue).toInt)
+  val generatedNodes = List(15, 190, 3, 91, 205, 171, 155, 303, 13, 17)
+  //  val generatedNodes = List.fill(nodesCount)((Math.random() * maxValue).toInt)
 
   val tree: BST = BST(root, generatedNodes)
 
   // add marker items
-  val testTree = tree + markerItem + markerItem2 + markerItem3
+  //  val testTree = tree + markerItem + markerItem2 + markerItem3
 
+  val l = tree.toList
+  print(tree.toString)
   // check that search is correct
-  require(testTree.find(markerItem).isDefined)
-  require(testTree.find(markerItem).isDefined)
-  require(testTree.find(markerItem).isDefined)
+  //  require(testTree.find(markerItem).isDefined)
+  //  require(testTree.find(markerItem).isDefined)
+  //  require(testTree.find(markerItem).isDefined)
 
-  println(testTree)
+  //  println(testTree)
 
 }
